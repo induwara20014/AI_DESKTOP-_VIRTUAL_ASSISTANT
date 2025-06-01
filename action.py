1830e128f46e020e4b0c7dfe1757b0a7ade051a8
induwara20014 <lakshaninduwara067@gmail.com>
2025-06-01 15:29:24 +0530

    Add speech recognition and text-to-speech functionality to AI Assistant

---
+++ action.py
@@ -0,0 +1,41 @@
+import text_to_speech
+import speech_to_text
+import datetime
+import webbrowser
+
+user_data = speech_to_text.speech_to_text()
+
+
+if "what is you name" in user_data :
+    text_to_speech.text_to_speech("My name is virtual assistant")
+
+elif "hello" in user_data or "hye" in user_data :
+    text_to_speech.text_to_speech("hey, sir How i can help you")
+
+elif "good morning" in user_data:
+        text_to_speech.text_to_speech("good morning sir")
+
+elif "time now" in user_data :
+    current_time = datetime.datetime.now()
+    Time = (str)(current_time) + "Hour : " ,(str)(current_time.minute) + "Minute"
+    text_to_speech.text_to_speech(Time)
+
+elif "shutdown" in user_data :
+    text_to_speech.text_to_speech("ok sir")
+
+elif "play music" in user_data :
+    webbrowser.open("https://gaana.com/")
+    text_to_speech.text_to_speech("gaana.com is now ready for you ")
+
+elif "youtube" in user_data :
+    webbrowser.open("https://youtube.com/")
+    text_to_speech.text_to_speech("youtube.com is now ready for you ")
+
+elif "open google" in user_data :
+    webbrowser.open("https://google.com/")
+    text_to_speech.text_to_speech("google.com is now ready for you ")
+
+
+
+else:
+    text_to_speech.text_to_speech("I'm not able to understand")
